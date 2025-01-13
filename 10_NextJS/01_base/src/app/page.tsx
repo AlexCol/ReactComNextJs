74a192a90af41fc864914905277b44a665faab69
AlexCol <eu_axil@yahoo.com.br>
2025-01-13 13:10:48 -0300

    .

---
+++ 10_NextJS/01_base/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/dist/client/link";
+
+export default function Home() {
+  return (
+    <div>
+      <p>Home Page</p>
+      <a href={"https://www.youtube.com/watch?v=e6FigV2fLC8"} target="_blank">Video com coisas Adicionais</a>
+    </div>
+  );
+}
