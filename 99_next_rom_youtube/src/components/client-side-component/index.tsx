310f179d4fb7d4f59b64ae85e1e3f678f9bf158e
AlexCol <eu_axil@yahoo.com.br>
2025-01-16 10:25:33 -0300

    .

--- 99_next_rom_youtube/src/components/client-side-component/index.tsx
+++ 99_next_rom_youtube/src/components/client-side-component/index.tsx
@@ -8,7 +8,10 @@ interface ButtonProps {
 }
 
 export function ClientSideComponent({ posts }: ButtonProps) {
+  const tailwindButton = 'bg-blue-500 hover:bg-red-700 text-white font-bold px-4 roundedtransition duration-500';
+
   const [nome, setNome] = useState('Alexandre');
+  const [clientPosts, setClientPosts] = useState<PostProps[]>(posts);
 
   function alterarNome() {
     if (nome === 'Alexandre') {
@@ -18,14 +21,24 @@ export function ClientSideComponent({ posts }: ButtonProps) {
     }
   }
 
+  const handlePosts = () => {
+    console.log("oi")
+    setClientPosts(clientPosts.filter((posts, index) => index !== 0));
+  }
+
   return (
     <div className="mt-5 mb-5 text-center bg-gray-200 p-4 rounded-md">
       <p>Hello. Im a client side component.</p>
       <button
         onClick={alterarNome}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        className={`${tailwindButton} py-2`}
       >Alterar nome: {nome}</button>
       <p>Total de posts: {posts.length}</p>
+      <p>Total de posts on client: {clientPosts.length}</p>
+      <button
+        className={tailwindButton}
+        onClick={handlePosts}
+      >Remove Post (works only on my, a client component</button>
     </div>
   )
 }
